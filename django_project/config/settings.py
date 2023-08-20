d1751b6fa2f086ce0fc934f2b1dce23538bb5db0
MiniGan067 <MiniGan067@yandex.ru>
2023-08-20 17:31:31 +0300

    fix

--- django_project/config/settings.py
+++ django_project/config/settings.py
@@ -49,8 +49,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'db'
     }
 }
 
